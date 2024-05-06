a69c9144b2dd574b3a1739e6e2bc60915ba4c796
zacccharv <zacchien1@gmail.com>
2024-05-05 21:12:21 -0400

    branch pruning

--- Assets/_Scripts/Scoring/HighScores.cs
+++ Assets/_Scripts/Scoring/HighScores.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Threading.Tasks;
 using Unity.Services.Leaderboards;
-using Unity.VisualScripting;
 using UnityEngine;
 
 [Serializable]
